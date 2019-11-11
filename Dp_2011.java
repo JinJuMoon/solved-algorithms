4381c29bd7991823c6e6c82ce73ea541247d356e
MOON JINJU <mjj920930@gmail.com>
2019-11-11 18:49:27 +0900

    백준 알고리즘 Dynamic Programming 문제풀이

---
+++ Dp_2011.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Dp_2011 {
+    static final int MOD = 1000000;
+    static int[] dp;
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String str = s.nextLine();
+        int length = str.length();
+        long[] dp = new long[length+1]; dp[0] = dp[1] = 1;
+
+        if(str.charAt(0) == '0') System.out.print(0);
+        else if(str.charAt(length-1) == '0' && str.charAt(length-2) != '1' && str.charAt(length-2) != '2') System.out.print(0);
+        else {
+            for(int i = 2; i <= length; i++) {
+                int tmp = Integer.parseInt(str.charAt(i-1) + "");
+                if(tmp > 0) dp[i] = dp[i-1] % 1000000;
+
+                tmp += Integer.parseInt(str.charAt(i-2) + "")*10;
+                if(10 <= tmp && tmp <= 26) dp[i] = (dp[i] + dp[i-2]) % 1000000;
+            }
+            System.out.print(dp[length]);
+        }
+    }
+}
